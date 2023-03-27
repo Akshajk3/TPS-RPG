84830a7bde1555c3a2ec17d3976c1ab84c0bebf5
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2023-03-26 21:42:51 -0700

    update

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float horizontal;
+    public float vertical;
+    public float moveAmount;
+    public float mouseX;
+    public float mouseY;
 
-    // Update is called once per frame
-    void Update()
+    PlayerControls inputActions;
+
+    Vector2 movementInput;
+    Vector2 cameraInput;
+
+
+    public void OnEnable()
     {
-        
+        if(inputActions == null)
+        {
+            inputActions = new PlayerControls();
+            inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
+        }
     }
 }
